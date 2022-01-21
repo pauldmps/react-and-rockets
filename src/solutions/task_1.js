bd2e82160b79036b2cf7070b88d9028821fa558a
Shantanu Paul <shantanupaul@live.com>
2022-01-21 12:17:14 +0100

    feat: task1 and task2 implementation

--- src/solutions/task_1.js
+++ src/solutions/task_1.js
@@ -1,3 +1,45 @@
 // Please implement your solution in this file
 
-export const prepareData = () => {};
+export const prepareData =
+  ({ year, customerName }) =>
+  (data) => {
+    const getPayloadCount = (item) => {
+      const {
+        rocket: { second_stage },
+      } = item;
+      return second_stage.payloads.length;
+    };
+
+    const filterFunction = (item) => {
+      const {
+        launch_year,
+        rocket: { second_stage },
+      } = item;
+      return (
+        Number(launch_year) === year &&
+        second_stage.payloads.find((payload) =>
+          payload.customers.find((customer) => customer.includes(customerName))
+        )
+      );
+    };
+
+    const sortFunction = (a, b) =>
+      getPayloadCount(b) - getPayloadCount(a) ||
+      b.flight_number - a.flight_number;
+
+    const outputFunction = (item) => {
+      const { flight_number, mission_name } = item;
+      return {
+        flight_number,
+        mission_name,
+        payloads_count: getPayloadCount(item),
+      };
+    };
+
+    const result = data
+      .filter(filterFunction)
+      .sort(sortFunction)
+      .map(outputFunction);
+
+    return result;
+  };
--- src/solutions/task_2.js
+++ src/solutions/task_2.js
@@ -1,3 +1,47 @@
 // Please implement your solution in this file
+import { prepareData } from "./task_1";
+import { useState, useEffect } from "react";
 
-export const RocketsList = () => <div>🚀 React & Rockets 🚀</div>;
+const API_URL = "https://api.spacexdata.com/v3/launches/past";
+
+const useApiData = (filterParams) => {
+  let [isLoading, setLoading] = useState(false);
+  let [responseJson, setResponseJson] = useState([]);
+  let [result, setResult] = useState([]);
+
+  const getApiData = async () => {
+    setLoading(true);
+    const response = await fetch(API_URL);
+    const responseJson = await response.json();
+    setResponseJson(responseJson);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getApiData();
+  }, []);
+
+  useEffect(() => {
+    setResult(prepareData(filterParams)(responseJson));
+  }, [responseJson, filterParams]);
+
+  return [isLoading, result];
+};
+
+export const RocketsList = ({ filterParams }) => {
+  const [isLoading, result] = useApiData(filterParams);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (result.length === 0) {
+    return <div>No data</div>;
+  }
+
+  return result.map((item) => (
+    <div
+      key={item.flight_number}
+    >{`#${item?.flight_number} ${item?.mission_name} (${item?.payloads_count})`}</div>
+  ));
+};
